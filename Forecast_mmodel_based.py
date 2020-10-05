82750a3febba8e0f6c08393fb10fbdf5e2e045f2
Mridusmita <67120587+Mridusmita167@users.noreply.github.com>
2020-10-05 19:56:50 +0530

    Add files via upload

---
+++ Forecast_mmodel_based.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("D:\\ExcelR\\Assignment\\Forecasting\\AirlinesData.csv")
+
+data.head()
+data.shape
+data.info()
+data.dtypes
+data.nunique()
+data.isnull().sum()
+
+#Stationarity
+data.Passengers.plot()
+
+#Creating dummy variables
+month =['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+p = data["Month"][0]
+p[0:3]
+data['months']= 0
+
+for i in range(96):
+    p = data["Month"][i]
+    data['months'][i]= p[0:3]
+    
+month_dummies = pd.DataFrame(pd.get_dummies(data['months']))
+data_n = pd.concat([data,month_dummies],axis = 1)
+
+data_n["t"] = np.arange(1,97)
+data_n["t_squared"] = data_n["t"]*data_n["t"]
+data_n.columns
+data_n["log_pass"] = np.log(data_n["Passengers"])
+
+#Splitting the train and test dataset
+Train = data_n.head(84)
+Test = data_n.tail(12)
+
+####################### L I N E A R ##########################
+import statsmodels.formula.api as smf 
+
+linear_model = smf.ols('Passengers~t',data=Train).fit()
+pred_linear =  pd.Series(linear_model.predict(pd.DataFrame(Test['t'])))
+rmse_linear = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_linear))**2))
+rmse_linear
+
+##################### Exponential ##############################
+
+Exp = smf.ols('log_pass~t',data=Train).fit()
+pred_Exp = pd.Series(Exp.predict(pd.DataFrame(Test['t'])))
+rmse_Exp = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(np.exp(pred_Exp)))**2))
+rmse_Exp
+
+#################### Quadratic ###############################
+
+Quad = smf.ols('Passengers~t+t_squared',data=Train).fit()
+pred_Quad = pd.Series(Quad.predict(Test[["t","t_squared"]]))
+rmse_Quad = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_Quad))**2))
+rmse_Quad
+
+################### Additive seasonality ########################
+
+add_sea = smf.ols('Passengers~Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data=Train).fit()
+pred_add_sea = pd.Series(add_sea.predict(Test[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov']]))
+rmse_add_sea = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_add_sea))**2))
+rmse_add_sea
+
+################## Additive Seasonality Quadratic ############################
+
+add_sea_Quad = smf.ols('Passengers~t+t_squared+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data=Train).fit()
+pred_add_sea_quad = pd.Series(add_sea_Quad.predict(Test[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','t','t_squared']]))
+rmse_add_sea_quad = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_add_sea_quad))**2))
+rmse_add_sea_quad 
+
+################## Multiplicative Seasonality ##################
+
+Mul_sea = smf.ols('log_pass~Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = Train).fit()
+pred_Mult_sea = pd.Series(Mul_sea.predict(Test))
+rmse_Mult_sea = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(np.exp(pred_Mult_sea)))**2))
+rmse_Mult_sea
+
+##################Multiplicative Additive Seasonality ###########
+
+Mul_Add_sea = smf.ols('log_pass~t+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = Train).fit()
+pred_Mult_add_sea = pd.Series(Mul_Add_sea.predict(Test))
+rmse_Mult_add_sea = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(np.exp(pred_Mult_add_sea)))**2))
+rmse_Mult_add_sea 
+
+################## Testing #######################################
+
+df= {"MODEL":pd.Series(["rmse_linear","rmse_Exp","rmse_Quad","rmse_add_sea","rmse_add_sea_quad","rmse_Mult_sea","rmse_Mult_add_sea"]),"RMSE_Values":pd.Series([rmse_linear,rmse_Exp,rmse_Quad,rmse_add_sea,rmse_add_sea_quad,rmse_Mult_sea,rmse_Mult_add_sea])}
+table_rmse=pd.DataFrame(df)
+table_rmse
+
+#Multiplicative additive seasonality has the least rmmse value
+
+model_full = smf.ols('log_pass~t+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = data_n).fit()
+
+pred_new  = pd.Series(Mul_Add_sea.predict(data_n))
+pred_new
+
+data["forecasted_passengers"] = pd.Series(pred_new)
+data
